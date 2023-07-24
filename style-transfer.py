f3ff1a496881906aa8184c1876824c64270890bb
javanasse <julian.vanasse@gmail.com>
2023-07-24 14:41:10 -0400

    WIP: adding feature extraction

--- style-transfer.py
+++ style-transfer.py
@@ -2,43 +2,33 @@ import torch
 import numpy as np
 from scipy.io import wavfile
 import numpy as np
+import soundfile as sf
+import torchcrepe
+from f0_extraction import get_f0_crepe
+from loudness_extraction import get_loudness
+from utils import normalize, vector2tensor
 
-# Parameters for pitch glissando
-duration = 0.5  # Duration in seconds
-sampling_rate = 44100  # Sampling rate (samples per second)
-num_samples = int(duration * sampling_rate)
-time = np.linspace(0, duration, num_samples)
+filename = "path/to/file.wav"
 
-# Parameters for loudness contour
-attack_time = 0.5  # Attack time in seconds
-decay_time = 0.05  # Decay time in seconds
+# read file
+audio, sample_rate = sf.read(filename)
+# force to mono
+audio = np.vstack((audio, audio)) if audio.shape[0] != 2 else audio
+# get dims
+num_samples = len(audio)
+duration = num_samples / sample_rate
+time = np.linspace(0, duration, num_samples, False)
 
-# Generate pitch glissando
-start_pitch = 200  # Starting pitch in Hz
-end_pitch = 400  # Ending pitch in Hz
-pitch = np.exp(np.linspace(np.log(start_pitch), np.log(end_pitch),
-                           num_samples))
+# extract f0
+pitch, _ = get_f0_crepe(audio, sample_rate)
 
-# Generate loudness contour
-loudness = np.ones_like(time)
-attack_samples = int(attack_time * sampling_rate)
-decay_samples = int(decay_time * sampling_rate)
+# extract loudness
+loudness = get_loudness(audio)
+# normalize
+loudness = normalize(loudness)
 
-# Apply exponential attack and decay to loudness
-attack_curve = np.linspace(0, 1, attack_samples)
-decay_curve = np.linspace(1, 0, decay_samples)
-loudness[:attack_samples] *= attack_curve
-loudness[-decay_samples:] *= decay_curve
-
-# Normalize loudness between 0 and 1
-loudness = (loudness - np.min(loudness)) / (np.max(loudness) -
-                                            np.min(loudness))
-
-pitch = torch.unsqueeze(torch.tensor(pitch), dim=1)
-loudness = torch.unsqueeze(torch.tensor(loudness), dim=1)
-
-pitch = torch.unsqueeze(pitch.to(torch.float32), dim=0)
-loudness = torch.unsqueeze(loudness.to(torch.float32), dim=0)
+pitch = vector2tensor(pitch)
+loudness = vector2tensor(loudness)
 
 model = torch.jit.load("./export/grimes/ddsp_grimes_pretrained.ts")
 
