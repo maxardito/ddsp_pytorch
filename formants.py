9889115eb4e1de897a71d79592025cfb0d43f898
javanasse <julian.vanasse@gmail.com>
2023-08-07 20:58:41 -0400

    WIP: more promising with praat+parselmouth

--- formants.py
+++ formants.py
@@ -2,8 +2,9 @@ import numpy as np
 import scipy.signal as sig
 import matplotlib.pyplot as plt
 import soundfile as sf
-import opensmile
-import pandas as pd
+import parselmouth
+from parselmouth import praat
+import librosa
 
 """
 Implementation of the CheapTrick spectral smoothing algorithm used for formant extraction.
@@ -58,34 +59,44 @@ t = np.linspace(0, dur, num_samples, False)
 
 # source audio
 f0 = 130
-formants = [400, 8000, 1500]
+formants = [400, 800, 1500]
 signal = source_filter_model(t, f0=f0, formants=formants)
 
-# analysis 
-# smile = opensmile.Smile(
-#     feature_set=opensmile.FeatureSet.ComParE_2016,
-#     feature_level=opensmile.FeatureLevel.Functionals,
-# )
-smile = opensmile.Smile(
-    feature_set=opensmile.FeatureSet.GeMAPSv01b,
-    feature_level=opensmile.FeatureLevel.LowLevelDescriptors,
-)
-smile.process_signal(
-    signal,
-    sample_rate
-)
+sf.write("tmp.wav", signal, sample_rate)
 
-data = smile.process_signal(signal, sample_rate)
+tmp = parselmouth.Sound("tmp.wav")
 
-centerformantfreqs = ['F1frequency_sma3nz', 'F2frequency_sma3nz', 'F3frequency_sma3nz']
-formant_df = data[centerformantfreqs]
+pointProcess = praat.call(tmp, "To PointProcess (periodic, cc)", 75, 400)
+formants = praat.call(tmp, "To Formant (burg)", 0.0025, 5, 5000, 0.025, 50)
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(formant_df)
+numPoints = praat.call(pointProcess, "Get number of points")
+f1_list = []
+f2_list = []
+f3_list = []
+for point in range(0, numPoints):
+    point += 1
+    t = praat.call(pointProcess, "Get time from index", point)
+    f1 = praat.call(formants, "Get value at time", 1, t, 'Hertz', 'Linear')
+    f2 = praat.call(formants, "Get value at time", 2, t, 'Hertz', 'Linear')
+    f3 = praat.call(formants, "Get value at time", 3, t, 'Hertz', 'Linear')
+    f1_list.append(f1)
+    f2_list.append(f2)
+    f3_list.append(f3)
+
+# signal_spectrum = np.fft.fft(signal)
+# signal_spectrum = signal_spectrum[:len(signal_spectrum)//2]
+# signal_spectrum = 20*np.log10(np.abs(signal_spectrum))
+# plt.plot(signal_spectrum)
+# plt.show()
+
+# signal_spectrogram = np.abs(librosa.stft(signal))
+# spect_t = np.linspace(0, dur, signal_spectrogram.shape[0])
+# spect_f = np.linspace(0, sample_rate/2, signal_spectrogram.shape[1], False)
+# signal_spectrogram = librosa.amplitude_to_db(signal_spectrogram, ref=np.max)
+# plt.pcolormesh(spect_t, spect_f, signal_spectrogram, shading='gouraud')
+
+f, t, Sxx = sig.spectrogram(signal, sample_rate)
+plt.pcolormesh(t, f, Sxx, shading='gouraud')
 
 
-signal_spectrum = np.fft.fft(signal)
-signal_spectrum = signal_spectrum[:len(signal_spectrum)//2]
-signal_spectrum = 20*np.log10(np.abs(signal_spectrum))
-plt.plot(signal_spectrum)
 plt.show()
